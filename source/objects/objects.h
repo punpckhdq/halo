a724a52d9eae1304d7edce2a3dd1a8d13e0647c5
ieee 802.11ac <69217234+ieee802dot11ac@users.noreply.github.com>
2026-01-10 17:50:33 -0800

    oops! (restore objs.c, objs.h)

--- source/objects/objects.h
+++ source/objects/objects.h
@@ -14,25 +14,7 @@ file has inline function assertions.
 
 /* ---------- structures */
 
-struct object_header
-{
-	byte pad0_pad8[8];
-	struct object* object;
-};
-
-struct object
-{
-	byte pad0_pad64[0x64];
-	short type_id;
-};
-
-/* ---------- prototypes/OBJECT.C */
-
-struct object* object_get_and_verify_type(long object_index, unsigned long type_mask);
-
-void object_export_function_values(long object_index);
-
-void object_render_debug(long object_index);
+/* ---------- prototypes/EXAMPLE.C */
 
 /* ---------- globals */
 
