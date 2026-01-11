a45b68af964706d03d3ec06bf944d5803b16db6f
ieee 802.11ac <69217234+ieee802dot11ac@users.noreply.github.com>
2026-01-10 16:08:04 -0800

    object stuff

--- source/objects/objects.h
+++ source/objects/objects.h
@@ -14,7 +14,25 @@ file has inline function assertions.
 
 /* ---------- structures */
 
-/* ---------- prototypes/EXAMPLE.C */
+struct object_header
+{
+	byte pad0_pad8[8];
+	struct object* object;
+};
+
+struct object
+{
+	byte pad0_pad64[0x64];
+	short type_id;
+};
+
+/* ---------- prototypes/OBJECT.C */
+
+struct object* object_get_and_verify_type(long object_index, unsigned long type_mask);
+
+void object_export_function_values(long object_index);
+
+void object_render_debug(long object_index);
 
 /* ---------- globals */
 
