ac8472e0d2a57eb061ee6c75e7505bdbc3e30759
punpckhdq <121350336+punpckhdq@users.noreply.github.com>
2026-06-16 12:47:09 +0100

    Update files.h

--- source/tag_files/files.h
+++ source/tag_files/files.h
@@ -184,7 +184,7 @@ boolean find_files_next(struct file_reference *file, struct file_last_modificati
 
 /* ---------- globals */
 
-char file_location_volume_names[NUMBER_OF_FILE_REFERENCE_LOCATIONS-1][256];
+extern char file_location_volume_names[NUMBER_OF_FILE_REFERENCE_LOCATIONS-1][256];
 
 /* ---------- public code */
 
